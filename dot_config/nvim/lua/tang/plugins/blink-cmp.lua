31112b52471196fea3ce7402c3ed724f1ea9d658
Richard Tang <richrdtang@gmail.com>
2026-09-04 18:06:51 -0700

    style blink-cmp

--- dot_config/nvim/lua/tang/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/tang/plugins/blink-cmp.lua
@@ -30,35 +30,30 @@ return {
 		},
 		completion = {
 			menu = {
+				border = "rounded",
 				draw = {
 					-- We don't need label_description now because label and label_description are already
 					-- combined together in label by colorful-menu.nvim.
-					columns = { { "kind_icon" }, { "label", gap = 1 } },
+					columns = { { "label", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
 					components = {
 						label = {
-							width = { fill = true, max = 60 },
-							text = function(ctx)
-								local highlights_info = require("colorful-menu").blink_highlights(ctx)
-								if highlights_info ~= nil then
-									-- Or you want to add more item to label
-									return highlights_info.label
-								else
-									return ctx.label
-								end
-							end,
-							highlight = function(ctx)
-								local highlights = {}
-								local highlights_info = require("colorful-menu").blink_highlights(ctx)
-								if highlights_info ~= nil then highlights = highlights_info.highlights end
-								for _, idx in ipairs(ctx.label_matched_indices) do
-									table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
-								end
-								-- Do something else
-								return highlights
-							end,
+							text = function(ctx) return require("colorful-menu").blink_components_text(ctx) end,
+							highlight = function(ctx) return require("colorful-menu").blink_components_highlight(ctx) end,
 						},
 					},
 				},
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 250,
+				window = {
+					border = "rounded",
+				},
+			},
+		},
+		signature = {
+			window = {
+				border = "rounded",
 			},
 		},
 	},
